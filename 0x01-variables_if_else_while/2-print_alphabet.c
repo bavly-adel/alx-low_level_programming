03452aad85ffc0e36dcaca74d69b9faa2b573276
bavly-adel <bavllyadell1998@gmail.com>
2023-06-16 17:58:53 -0700

    first

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,11 @@
 
 int main(void) 
 {
-    int i;
-    for (i = 97; i <= 122; i++) 
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return 0;
+int i;
+for (i = 97; i <= 122; i++) 
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
 }
