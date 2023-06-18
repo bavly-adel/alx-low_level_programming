638e36aa8b423150d1b030961acad4000866484d
bavly-adel <bavllyadell1998@gmail.com>
2023-06-18 08:02:32 -0700

    first23

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+int main(void)
+{
 /**
 *main: print message using puts
 *Return: 0 (Success)
 */
-int main(void)
-{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
