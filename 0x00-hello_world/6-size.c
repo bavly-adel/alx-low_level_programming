6ab58067803fd2ae47086d69c5bf1ce9887f891c
bavly-adel <bavllyadell1998@gmail.com>
2023-06-18 09:04:55 -0700

    first23

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,10 @@
 */
 int main(void)
 {
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long: %zu bytes\n", sizeof(long));
-printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of short: %zu bytes\n", sizeof(short));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes\n", sizeof(int));
+printf("Size of a long int: %zu bytes\n", sizeof(long));
+printf("Size of a long long int: %zu bytes\n", sizeof(long long));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
