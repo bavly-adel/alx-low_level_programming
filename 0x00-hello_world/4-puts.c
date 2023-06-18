644aa862bf44c3e2f24cceb2e72aed053c88e48e
bavly-adel <bavllyadell1998@gmail.com>
2023-06-18 07:35:47 -0700

    first2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *A program to print message using puts instead of printf 
+ *Retturn 0 (success)
+ * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
