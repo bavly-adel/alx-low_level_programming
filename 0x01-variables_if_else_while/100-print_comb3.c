29f54f151756a76b8033d9b4f3df2cc2171a0cc6
bavly-adel <bavllyadell1998@gmail.com>
2023-06-19 08:54:52 -0700

    first

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+int j, i;
+for (j = 0; j < 10; j++)
+{
+for(i = j+1; i < 10; i++)
+{
+putchar((char)(j + '0'));
+putchar((char)(i + '0'));
+
+if (i == 9 && j == 8)
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
