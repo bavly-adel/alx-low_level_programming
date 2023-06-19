2fc15edc76c8db52b0a5a340f5f29ec2c3225285
bavly-adel <bavllyadell1998@gmail.com>
2023-06-19 05:02:53 -0700

    firt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-/*main- print using write*/
+/*
+*main: print using write
+*Return: 0 (Success)
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
