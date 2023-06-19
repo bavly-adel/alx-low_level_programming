4f7cd5835fff3518d5455b4f7897cf8640893eb8
bavly-adel <bavllyadell1998@gmail.com>
2023-06-19 09:01:11 -0700

    first

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/*
+*main: print combination of numbers
+*Return: 0 (Success)
+*/
 int main(void)
 {
 int j, i;
 for (j = 0; j < 10; j++)
 {
-for(i = j+1; i < 10; i++)
+for (i = j + 1; i < 10; i++)
 {
 putchar((char)(j + '0'));
 putchar((char)(i + '0'));
