a06dd1da942f3c864eca951f6b1edb35de70cfb0
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 04:39:24 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/*
+* main - Entry point for this program
+* This function prints "_putchar"
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void) {
+char c;
+int i=0;
+while (i<10)
+{
+for (c = 'a'; c <= 'z'; c++) {
+putchar(c);
+}
+i++;
+}
+putchar('\n');
+}
