5fdf4262506b5c4239e19af444d644565f3bb5b9
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 04:50:19 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,18 @@
 * This function prints "_putchar"
 * Return: Always 0 (Success)
 */
-void print_alphabet_x10(void) {
+void print_alphabet_x10(void)
+{
 char c;
 int i=0;
 while (i<10)
 {
-for (c = 'a'; c <= 'z'; c++) {
+for (c = 'a'; c <= 'z'; c++)
+{
 putchar(c);
 }
 i++;
+putchar('\n');
 }
 putchar('\n');
 }
