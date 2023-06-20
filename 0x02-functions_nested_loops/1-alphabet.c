ae32e83e48f0712a99afc5b04fdb301fb808ee4a
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 03:02:01 -0700

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+* main - Entry point for this program
+* This function prints "_putchar"
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for(i=97,i<123,i++)
+{
+putchar(i);
+printf("\n");
+return (0);
+}
