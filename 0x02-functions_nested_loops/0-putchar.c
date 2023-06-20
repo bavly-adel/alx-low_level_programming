895edca031ffec56b6b726f37d1c1f73c32487d3
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 02:41:23 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /*
 * main - Entry point for this program
+* This function prints the string "_putchar" to the console using the printf function
 * Return: Always 0 (Success)
 */
 int main(void)
