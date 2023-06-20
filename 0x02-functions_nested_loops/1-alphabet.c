3a8475b3fdedba560d1fc70e83c8fa4ebea6397e
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 03:35:33 -0700

    first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,9 @@
 * Return: Always 0 (Success)
 */
 void print_alphabet(void) {
-    int i;
-    for (i = 'a'; i <= 'z'; i++) {
-        putchar(i);
+    char c;
+    for (c = 'a'; c <= 'z'; c++) {
+        _putchar(c);
     }
-}
-
-int main(void) {
-    print_alphabet();
-    return 0;
+    _putchar('\n');
 }
