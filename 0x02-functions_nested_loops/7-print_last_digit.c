8ea0aee8b311ff8a384661c646758d7b97e173c1
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 19:02:32 -0700

    first

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/*
+* main - Entry point for this program
+* This function prints "_putchar"
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return(0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _islower(int c);
+void _putchar(char i);
 int main(void);
