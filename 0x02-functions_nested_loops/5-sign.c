22bf3ae10418c9944081a9866260a76dc0197c40
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 19:17:26 -0700

    first

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
 return (1);
+puts("+");
 }
 else if (n < 0)
 {
-printf("-");
 return (-1);
+puts("-");
 }
 else
 {
-printf("0");
 return (0);
+puts("0")
 }
 }
