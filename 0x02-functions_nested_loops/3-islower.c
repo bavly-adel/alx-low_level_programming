17ecceb2d4b4df041d427da6f5c86f6c54f28d0d
bavly-adel <bavllyadell1998@gmail.com>
2023-06-20 19:05:06 -0700

    first

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,14 @@
 * This function prints "_putchar"
 * Return: Always 0 (Success)
 */
-int _islower(int c)
+int _islower (int c)
 {
-if(c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
 else
 {
-return(0);
+return (0);
 }
 }
