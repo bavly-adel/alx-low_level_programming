10760d92fb65652ff45cc3364b874a0e4ac797bf
bavly-adel <bavllyadell1998@gmail.com>
2023-06-21 19:50:16 -0700

    first

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 void print_times_table(int n) {
-  // Check if n is within the allowed range.
   if (n < 0 || n > 15) {
     return;
   }
 
-  int i, j;
+  int i;
+  int j;
 
   for (i = 0; i <= n; i++) {
     for (j = 0; j <= n; j++) {
-      // Multiply i by j to get the table value, and print it.
       printf("%4d", i * j);
     }
-    // Print a newline character after each row.
     printf("\n");
   }
 }
