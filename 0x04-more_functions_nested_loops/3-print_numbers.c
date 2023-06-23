a1ab68f84d3fdd788a16aeb118d8f599b21ffba0
bavly-adel <bavllyadell1998@gmail.com>
2023-06-22 17:35:38 -0700

    first

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include "main.h"
 /*
-* printt_numbers- chech if digit
+* print_numbers- chech if digit
 * Return: 1 (if digit) 0 (else)
 */
 void print_numbers(void)
@@ -9,6 +9,6 @@ void print_numbers(void)
 int i;
 for (i = 0; i <= 9; i++)
 {
-putchar(i);
+putchar(i + '0');
 putchar('\n');
 }
