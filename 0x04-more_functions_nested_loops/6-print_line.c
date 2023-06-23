2e2db22372bac2ae486cc8448cc1abd4b21b8e1b
bavly-adel <bavllyadell1998@gmail.com>
2023-06-22 18:23:09 -0700

    first

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+/*
+* print_line - print digits
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+while (n > 0)
+{
+putchar('_');
+}
+putchar('\n');
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-void more_numbers(void);
+void print_line(int n);
 int main(void);
