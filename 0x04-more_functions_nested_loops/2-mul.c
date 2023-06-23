51b777cd86b694f7ee8f707c98e452827d7a2913
bavly-adel <bavllyadell1998@gmail.com>
2023-06-22 17:21:49 -0700

    first

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "main.h"
+/*
+* mul - chech if digit
+* Return: a * b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-int _isdigit(int c);
+int mul(int a, int b);
 int main(void);
