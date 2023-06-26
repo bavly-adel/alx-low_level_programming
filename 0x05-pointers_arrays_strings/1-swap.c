a8592d08c8edd750dc0785882515e5d0ec106a46
bavly-adel <bavllyadell1998@gmail.com>
2023-06-26 07:05:41 -0700

    first

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - swap to integers
+* @a: pointer to first int
+* @b: pointer to second int
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = a;
+*a = b;
+*b = temp;
+}
