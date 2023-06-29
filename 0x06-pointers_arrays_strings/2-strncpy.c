00553cde652f74fd523fe1a32b21993ceadf21f9
bavly-adel <bavllyadell1998@gmail.com>
2023-06-29 14:17:48 -0700

    first

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-int dest_len = 0;
-while (dest[dest_len] != '\0')
-{
-dest_len++;
-}
 for (i = 0; i < n && src[i] != '\0'; i++)
 {
-dest[dest_len + i] = src[i];
+dest[i] = src[i];
 }
-dest[dest_len + i] = '\0';
+for (; i < n; i++) {
+dest[i] = '\0';
+}
 return (dest);
 }
