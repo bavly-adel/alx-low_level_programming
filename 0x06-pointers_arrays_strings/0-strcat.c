1e0c5a7e65f38f624801c0d1b4c9281007f37183
bavly-adel <bavllyadell1998@gmail.com>
2023-06-29 14:05:32 -0700

    first

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,20 +16,5 @@ for (i = 0; src[i] != '\0'; i++) {
 dest[dest_len + i] = src[i];
 }
 dest[dest_len + i] = '\0';
-return dest;
-}
-
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+return (dest);
 }
