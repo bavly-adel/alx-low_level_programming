14d92c04b1a91f10faf79651c2adfdbd5c409a31
bavly-adel <bavllyadell1998@gmail.com>
2023-06-29 14:08:32 -0700

    first

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,12 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int dest_len = 0;
-while (dest[dest_len] != '\0') {
+while (dest[dest_len] != '\0')
+{
 dest_len++;
 }
-for (i = 0; src[i] != '\0'; i++) {
+for (i = 0; src[i] != '\0'; i++)
+{
 dest[dest_len + i] = src[i];
 }
 dest[dest_len + i] = '\0';
