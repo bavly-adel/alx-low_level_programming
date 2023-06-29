1367136976073e4b4eb0df2f7cda9e2ad05109c4
bavly-adel <bavllyadell1998@gmail.com>
2023-06-29 14:10:14 -0700

    first

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * *_strncat - the function name
+ * @dest : desttination parameter
+ * @src : source parameter
+ * @n : number as parameter
+ * Return: string after concat
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int dest_len = 0;
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[dest_len + i] = src[i];
+}
+dest[dest_len + i] = '\0';
+return (dest);
+}
