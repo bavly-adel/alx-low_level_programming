16991653529065d95deb924277ca29dcb98837b4
bavly-adel <bavllyadell1998@gmail.com>
2023-06-29 15:17:22 -0700

    first

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,9 +3,10 @@
  * *_strcmp - the function name
  * @s2 : desttination parameter
  * @s1 : source parameter
- * Return: string after concat
+ * Return : string after concat
  */
-int _strcmp(char *s1, char *s2) {
+int _strcmp(char *s1, char *s2)
+{
 int i = 0;
 while (s1[i] == s2[i])
 {
