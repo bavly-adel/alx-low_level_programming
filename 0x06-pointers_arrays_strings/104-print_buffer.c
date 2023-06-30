7f75026580861448e368ff5ccb80f59881fac093
bavly-adel <bavllyadell1998@gmail.com>
2023-06-30 09:53:57 -0700

    first

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,11 +2,12 @@
 #include <ctype.h>
 
 void print_buffer(char *b, int size) {
-    if (size <= 0) {
-        printf("\n");
-        return;
-    }
-    int i, j;
+int i;
+int j;
+if (size <= 0) {
+printf("\n");
+return;
+}
     for (i = 0; i < size; i += 10) {
         printf("%08x ", i);
         for (j = 0; j < 10; j++) {
