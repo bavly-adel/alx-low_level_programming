328c13ccc7e79f3b8c6406d7505b94e6aa8d9632
bavly-adel <bavllyadell1998@gmail.com>
2023-06-30 10:02:51 -0700

    first

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <ctype.h>
+/**
+ * print_buffer - func name
+ * @b : first para
+ * @size : size param
+ */
 
-void print_buffer(char *b, int size) {
+void print_buffer(char *b, int size)
+{
 int i;
 int j;
-if (size <= 0) {
+if (size <= 0)
+{
 printf("\n");
 return;
 }
-    for (i = 0; i < size; i += 10) {
-        printf("%08x ", i);
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                printf("%02x ", (unsigned char)b[i + j]);
-            } else {
-                printf("   ");
-            }
-        }
-        printf(" ");
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
-            }
-        }
-        printf("\n");
-    }
+for (i =  0; i < size; i += 10)
+{
+printf("%08x ", i);
+for (j = 0; j < 10; j++) 
+{
+if (i + j < size)
+{
+printf("%02x ", (unsigned char)b[i + j]);
+}
+else
+{
+printf("   ");
+}
+}
+printf(" ");
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
+}
+}
+printf("\n");
+}
 }
 
