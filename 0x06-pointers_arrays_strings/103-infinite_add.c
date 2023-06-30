fa16c410f6b5254af970ad78202593d4e5d63bc5
bavly-adel <bavllyadell1998@gmail.com>
2023-06-30 05:52:53 -0700

    first

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,17 +9,26 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = strlen(n1);
-int len2 = strlen(n2);
+int len1;
+int len2;
+int digit1;
+int digit2;
+int carry;
+len1 = strlen(n1);
+len2 = strlen(n2);
 if (len1 >= size_r || len2 >= size_r)
 {
-return 0;
+return (0);
 }
-int carry = 0, sum = 0, i = len1 - 1, j = len2 - 1, k = size_r - 1;
+carry = 0;
+sum = 0;
+i = len1 - 1;
+j = len2 - 1;
+k = size_r - 1;
 while (i >= 0 || j >= 0 || carry > 0)
 {
-int digit1 = i >= 0 ? n1[i] - '0' : 0;
-int digit2 = j >= 0 ? n2[j] - '0' : 0;
+digit1 = i >= 0 ? n1[i] - '0' : 0;
+digit2 = j >= 0 ? n2[j] - '0' : 0;
 sum = digit1 + digit2 + carry;
 if (sum >= 10)
 {
