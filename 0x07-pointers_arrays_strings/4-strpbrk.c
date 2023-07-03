02322b5cc0f8f6324a1d1426aeac3bbe76e93dfb
bavly-adel <bavllyadell1998@gmail.com>
2023-07-03 02:22:17 -0700

    firts

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 /**
- * _strpbrk - function name
+ * *_strpbrk - function name
  * @s: pointer s to a memory area
  * @accept: constant byte b
  * Return : pointer to the memory area s
  */
 char *_strpbrk(char *s, char *accept)
 {
+char *ptr;
 while (*s != '\0')
 {
-char *ptr = accept;
+*ptr = accept;
 while (*ptr != '\0')
 {
 if (*s == *ptr)
