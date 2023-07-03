2155f922816b9e27832d434222060c29e6c67361
bavly-adel <bavllyadell1998@gmail.com>
2023-07-03 06:35:46 -0700

    first

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * print_chessboard - function name
- * @a: pointer s to a memory area
+ * @a : pointer s to a memory area
  */
 void print_chessboard(char (*a)[8])
 {
@@ -11,8 +11,8 @@ for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-printf("%c ", a[i][j]);
+putchar(a[i][j]);
 }
-printf("\n");
+putchar('\n');
 }
 }
