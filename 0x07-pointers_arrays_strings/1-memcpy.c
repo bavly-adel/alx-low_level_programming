b57a9bde25358a6e3e0a51eb59d034c960370e0e
bavly-adel <bavllyadell1998@gmail.com>
2023-07-03 03:59:07 -0700

    first

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: number of bytes to fill
  * Return : pointer to the memory area s
  */
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int i;
 for (i = 0; i < n; i++)
