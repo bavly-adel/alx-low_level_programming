16d9043069bfaaeaa1764e25c4d646642131a27f
bavly-adel <bavllyadell1998@gmail.com>
2023-07-08 05:36:11 -0700

    ff

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - func name
+ * argc : count of arguments
+ * argv : array of arguments
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return 0;
+printf("%s\n", argv[0]);
+return (0);
 }
