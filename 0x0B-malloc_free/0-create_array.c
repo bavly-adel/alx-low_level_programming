107d904a9dacf7bc027ad54d726e93cfbdd2a7af
bavly-adel <bavllyadell1998@gmail.com>
2023-07-11 15:02:12 -0700

    f

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,13 +6,13 @@
  * Return: char arr point
  */
 char *create_array(unsigned int size, char c) {
-char arr;
+char *arr;
 unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
-*arr = malloc(size * sizeof(char));
+arr = malloc(size * sizeof(char));
 if (arr == NULL)
 {
 return (NULL);
